07e77f21e024db9002e172d0792d0da0c15c9e40
Hesham Massoud <hesham.massoud@commercetools.de>
2018-06-21 10:59:46 +0200

    Send reply message in the conversation rather than privatley.

--- src/main/java/com/heshammassoud/service/stride/MessageService.java
+++ src/main/java/com/heshammassoud/service/stride/MessageService.java
@@ -3,6 +3,7 @@ package com.heshammassoud.service.stride;
 import com.atlassian.adf.Document;
 import com.atlassian.stride.api.StrideClient;
 import com.atlassian.stride.api.model.EntityCreatedResponse;
+import com.atlassian.stride.model.context.ConversationContext;
 import com.atlassian.stride.model.context.UserContext;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,21 @@ public class MessageService {
         return strideClient.message()
                            .send(message)
                            .toUser(context);
+    }
+
+    /**
+     * Sends the passed {@code message} to the stride conversation defined by the passed {@link UserContext}.
+     *
+     * @param context the conversation context.
+     * @param message the message to send.
+     * @return a future containing the result of sending the message.
+     */
+    @Nonnull
+    public CompletableFuture<EntityCreatedResponse> send(@Nonnull final ConversationContext context,
+                                                         @Nonnull final Document message) {
+        return strideClient.message()
+                           .send(message)
+                           .toConversation(context);
     }
 
 }
--- src/main/java/com/heshammassoud/service/stride/ReplierService.java
+++ src/main/java/com/heshammassoud/service/stride/ReplierService.java
@@ -1,12 +1,12 @@
 package com.heshammassoud.service.stride;
 
-import com.atlassian.stride.model.context.UserContext;
+import com.atlassian.stride.model.context.ConversationContext;
 import com.atlassian.stride.model.webhooks.MessageSent;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
 
-import static com.heshammassoud.util.stride.ContextUtil.toUserContext;
+import static com.heshammassoud.util.stride.ContextUtil.toConversationContext;
 import static com.heshammassoud.util.stride.MessageUtil.getMainMenuReply;
 
 @Service
@@ -28,11 +28,14 @@ public class ReplierService {
      */
     public void mainMenuReply(@Nonnull final MessageSent messageSent) {
         final String messageContent = messageSent.getMessage().getText();
-        final UserContext userContext = toUserContext(messageSent);
+        final ConversationContext context = toConversationContext(messageSent);
 
-        userService.getUser(userContext) // 1. get user.
-                   .thenApply(userDetail -> getMainMenuReply(messageContent, userDetail)) // 2. build reply.
-                   .thenCompose(reply -> messageService.sendPrivately(userContext, reply)); // 3. send message.
+        messageService.send(context, getMainMenuReply(messageContent, null));
+        /**
+         *userService.getUser(userContext) // 1. get user.
+         .thenApply(userDetail -> getMainMenuReply(messageContent, userDetail)) // 2. build reply.
+         .thenCompose(reply -> messageService.sendPrivately(userContext, reply)); // 3. send message.
+         */
     }
 
 }
