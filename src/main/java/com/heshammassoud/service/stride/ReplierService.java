e5313d7018b917a1adc96af1c60a51c00d617ecf
Hesham Massoud <hesham.massoud@commercetools.de>
2018-06-21 11:24:03 +0200

    Add "Hi" menu.

--- src/main/java/com/heshammassoud/service/stride/ReplierService.java
+++ src/main/java/com/heshammassoud/service/stride/ReplierService.java
@@ -1,13 +1,18 @@
 package com.heshammassoud.service.stride;
 
+import com.atlassian.stride.api.model.UserDetail;
 import com.atlassian.stride.model.context.ConversationContext;
+import com.atlassian.stride.model.context.UserContext;
 import com.atlassian.stride.model.webhooks.MessageSent;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
 
 import static com.heshammassoud.util.stride.ContextUtil.toConversationContext;
+import static com.heshammassoud.util.stride.ContextUtil.toUserContext;
 import static com.heshammassoud.util.stride.MessageUtil.getMainMenuReply;
+import static com.heshammassoud.util.stride.MessageUtil.mainMenu;
+import static java.lang.String.format;
 
 @Service
 public class ReplierService {
@@ -28,14 +33,22 @@ public class ReplierService {
      */
     public void mainMenuReply(@Nonnull final MessageSent messageSent) {
         final String messageContent = messageSent.getMessage().getText();
-        final ConversationContext context = toConversationContext(messageSent);
+        final ConversationContext conversationContext = toConversationContext(messageSent);
+        final UserContext userContext = toUserContext(messageSent);
 
-        messageService.send(context, getMainMenuReply(messageContent, null));
-        /**
-         *userService.getUser(userContext) // 1. get user.
-         .thenApply(userDetail -> getMainMenuReply(messageContent, userDetail)) // 2. build reply.
-         .thenCompose(reply -> messageService.sendPrivately(userContext, reply)); // 3. send message.
-         */
+
+        if (messageContent.toLowerCase().contains("hi ")) {
+            userService.getUser(userContext) // 1. get user.
+                       .thenApply(UserDetail::getDisplayName)
+                       .thenApply(displayName -> mainMenu(
+                           format("Hi %s, hope you are having a great day! What can I do for you?",
+                               displayName))) // 2. build reply.
+                       .thenCompose(reply -> messageService.sendPrivately(userContext, reply)); // 3. send message.
+        } else {
+            messageService.send(conversationContext, getMainMenuReply(messageContent, null));
+        }
+
+
     }
 
 }
