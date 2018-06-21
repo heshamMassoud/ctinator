3dba480847ba0bcdee4051f98236dc9637e216c7
Hesham Massoud <hesham.massoud@commercetools.de>
2018-06-21 16:01:44 +0200

    Never get mapping.

--- src/main/java/com/heshammassoud/controller/DialogController.java
+++ src/main/java/com/heshammassoud/controller/DialogController.java
@@ -1,17 +1,12 @@
 package com.heshammassoud.controller;
 
 
-import com.atlassian.stride.spring.auth.AuthorizeJwtHeader;
-import com.heshammassoud.models.ActionResponse;
-import com.heshammassoud.models.ActionTargetRequest;
 import com.heshammassoud.service.commercetools.ProductService;
 import io.sphere.sdk.products.Product;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Nonnull;
@@ -27,14 +22,13 @@ public class DialogController {
         this.productService = productService;
     }
 
-    /*    *
+    /**
      * Defines the descriptor endpoint for describing the bot json.
      *
      * @return a {@link ModelAndView} containing the appName and the serviceConfig.
-
-    @GetMapping( {"/productdialog", "/productdialog/"})
-    @ResponseStatus(HttpStatus.OK)
-    public String renderDialogView(@RequestParam("id") final String id, @Nonnull final Model model) {
+     */
+    @GetMapping( {"/productdialog/{id}/", "/productdialog/{id}"})
+    public String renderDialogView(@RequestParam("id") String id, @Nonnull final Model model) {
 
         final Product product = productService.getProductById(id).join();
         model.addAttribute("productName", product.getMasterData().getCurrent().getName().get(ENGLISH));
@@ -44,23 +38,6 @@ public class DialogController {
             "https://impex.commercetools.com/playground?endpoint=products&method=read-id&resourceId="
                 + id);
         return "productdialog";
-    }*/
-
-    /**
-     * Defines the descriptor endpoint for describing the bot json.
-     *
-     * @return a {@link ModelAndView} containing the appName and the serviceConfig.
-     */
-    @AuthorizeJwtHeader
-    @PostMapping(path = {"/productdialog", "/productdialog/"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public ActionResponse renderDialogView(@RequestBody @Nonnull final ActionTargetRequest actionTargetRequest) {
-
-        final String id = (String) actionTargetRequest.getParameters().get("id");
-
-        final Product product = productService.getProductById(id).join();
-        final String name = product.getMasterData().getCurrent().getName().get(ENGLISH);
-        return name == null ? ActionResponse.of() : ActionResponse.ofMessage(name);
     }
 
 }
