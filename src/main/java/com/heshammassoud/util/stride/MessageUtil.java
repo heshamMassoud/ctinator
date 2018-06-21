cbb0026009348308038b9752588d4ce8014fe055
Hesham Massoud <hesham.massoud@commercetools.de>
2018-06-21 10:37:43 +0200

    Fix formatting.

--- src/main/java/com/heshammassoud/util/stride/MessageUtil.java
+++ src/main/java/com/heshammassoud/util/stride/MessageUtil.java
@@ -19,7 +19,8 @@ public final class MessageUtil {
 
     /**
      * Builds the bot's main menu with a custom header according to the user's initial message to the bot.
-     * @param message the initial message sent by the user.
+     *
+     * @param message    the initial message sent by the user.
      * @param userDetail the details of the user to receive the reply.
      * @return a built main menu {@link Document} with a custom header.
      */
@@ -33,7 +34,7 @@ public final class MessageUtil {
         final String patternString = "(show|view).*(product.*)";
         final Pattern pattern = Pattern.compile(patternString);
         final Matcher matcher = pattern.matcher(message);
-        if(matcher.matches()) {
+        if (matcher.matches()) {
             return referenceDocument();
         }
 
@@ -42,38 +43,39 @@ public final class MessageUtil {
             header = format("Hi %s, hope you are having a great day! What can I do for you?", userDisplayName);
         } else {
             header = (format("Hi %s, sorry I didn't get that. However, can I help you with one of these things?",
-                    userDisplayName));
+                userDisplayName));
         }
         return mainMenu(header);
     }
 
     /**
      * Builds the bot's main menu with a custom header.
+     *
      * @param menuHeader the header to prepend to the bot's main menu.
      * @return a built main menu {@link Document} with a custom header.
      */
     @Nonnull
     public static Document mainMenu(@Nonnull final String menuHeader) {
         final ActionGroupAction syncMenuOption =
-                ActionGroupAction.of("sync-menu-option",
-                        "Sync data between two commercetools projects.",
-                        "primary", "syncMenuOption", emptyMap());
+            ActionGroupAction.of("sync-menu-option",
+                "Sync data between two commercetools projects.",
+                "primary", "syncMenuOption", emptyMap());
 
         final ActionGroupAction deleteMenuOption =
-                ActionGroupAction.of("delete-menu-option",
-                        "Delete all data of one resource (e.g. products, categories, etc..)"
-                                + " in a commercetools project.",
-                        "primary", "deleteMenuOption", emptyMap());
+            ActionGroupAction.of("delete-menu-option",
+                "Delete all data of one resource (e.g. products, categories, etc..)"
+                    + " in a commercetools project.",
+                "primary", "deleteMenuOption", emptyMap());
 
         final ActionGroupAction viewMenuOption =
-                ActionGroupAction.of("view-menu-option",
-                        "View data in a commercetools project.",
-                        "primary", "viewMenuOption", emptyMap());
+            ActionGroupAction.of("view-menu-option",
+                "View data in a commercetools project.",
+                "primary", "viewMenuOption", emptyMap());
 
         final InlineExtension mainMenuActionGroup = ofActionGroup("mainMenu",
-                syncMenuOption,
-                deleteMenuOption,
-                viewMenuOption);
+            syncMenuOption,
+            deleteMenuOption,
+            viewMenuOption);
 
         return Document.create()
                        .paragraph(paragraph -> paragraph.text(menuHeader))
@@ -82,6 +84,7 @@ public final class MessageUtil {
 
     /**
      * Builds the bot's delete menu.
+     *
      * @return a built main menu {@link Document} with a custom header.
      */
     @Nonnull
@@ -89,24 +92,24 @@ public final class MessageUtil {
 
         final String menuHeader = "Right now, I only know how to delete for you the following: ";
         final ActionGroupAction productsDeleteOption =
-                ActionGroupAction.of("products-delete-option",
-                        "Products",
-                        "default", "productsDeleteOption", emptyMap());
+            ActionGroupAction.of("products-delete-option",
+                "Products",
+                "default", "productsDeleteOption", emptyMap());
 
         final ActionGroupAction categoriesDeleteOption =
-                ActionGroupAction.of("categories-delete-option",
-                        "Categories",
-                        "default", "categoriesDeleteOption", emptyMap());
+            ActionGroupAction.of("categories-delete-option",
+                "Categories",
+                "default", "categoriesDeleteOption", emptyMap());
 
         final ActionGroupAction inventoriesDeleteOption =
-                ActionGroupAction.of("inventories-delete-option",
-                        "Inventory Entries",
-                        "default", "inventoriesDeleteOption", emptyMap());
+            ActionGroupAction.of("inventories-delete-option",
+                "Inventory Entries",
+                "default", "inventoriesDeleteOption", emptyMap());
 
         final InlineExtension deleteMenuActionGroup = ofActionGroup("deleteMenu",
-                productsDeleteOption,
-                categoriesDeleteOption,
-                inventoriesDeleteOption);
+            productsDeleteOption,
+            categoriesDeleteOption,
+            inventoriesDeleteOption);
 
         return Document.create()
                        .paragraph(paragraph -> paragraph.text(menuHeader))
@@ -115,6 +118,7 @@ public final class MessageUtil {
 
     /**
      * Builds the bot's delete menu.
+     *
      * @return a built main menu {@link Document} with a custom header.
      */
     @Nonnull
@@ -122,22 +126,21 @@ public final class MessageUtil {
                                                  final int numberOfProducts) {
 
         final String menuHeader =
-                format("Are you sure you want to delete all %d products in '%s'?", numberOfProducts, projectKey);
+            format("Are you sure you want to delete all %d products in '%s'?", numberOfProducts, projectKey);
 
         final ActionGroupAction yesOption =
-                ActionGroupAction.of("products-delete-yes-confirm-option",
-                        "Yes",
-                        "default", "productsDeleteYes", emptyMap());
+            ActionGroupAction.of("products-delete-yes-confirm-option",
+                "Yes",
+                "default", "productsDeleteYes", emptyMap());
 
         final ActionGroupAction noOption =
-                ActionGroupAction.of("products-delete-no-confirm-option",
-                        "No",
-                        "default", "productsDeleteNo", emptyMap());
-
+            ActionGroupAction.of("products-delete-no-confirm-option",
+                "No",
+                "default", "productsDeleteNo", emptyMap());
 
 
         final InlineExtension confirmationActionGroup =
-                ofActionGroup("deleteProductsConfirm", yesOption, noOption);
+            ofActionGroup("deleteProductsConfirm", yesOption, noOption);
 
         return Document.create()
                        .paragraph(paragraph -> paragraph.text(menuHeader))
@@ -146,30 +149,33 @@ public final class MessageUtil {
 
     /**
      * A reference menu to use stuff from.
+     *
      * @return a built reference menu.
      */
     @Nonnull
     private static Document referenceDocument() {
         return Document.create()
                        .paragraph(p -> p
-                               .text("In the future (when I am smarter) I would be asking you for the product Id or key in commercetools, but now I will just show you some random stuff..")
-                               .strong("bold test")
-                               .text(" and ")
-                               .em("text in italics")
-                               .text(" as well as ")
-                               .link(" a link", "https://www.atlassian.com")
-                               .text(" , emojis ")
-                               .emoji(":smile:")
-                               .emoji(":rofl:")
-                               .emoji(":nerd:")
-                               .text(" and some code: ")
-                               .code("var i = 0;")
-                               .text(" and a bullet list"))
+                           .text(
+                               "In the future (when I am smarter) I would be asking you for the product Id or key in"
+                                   + " commercetools, but now I will just show you some random stuff..")
+                           .strong("bold test")
+                           .text(" and ")
+                           .em("text in italics")
+                           .text(" as well as ")
+                           .link(" a link", "https://www.atlassian.com")
+                           .text(" , emojis ")
+                           .emoji(":smile:")
+                           .emoji(":rofl:")
+                           .emoji(":nerd:")
+                           .text(" and some code: ")
+                           .code("var i = 0;")
+                           .text(" and a bullet list"))
                        .bulletList(b -> b
-                               .item("With one bullet point")
-                               .item("And another"))
+                           .item("With one bullet point")
+                           .item("And another"))
                        .info(p -> p
-                               .paragraph("and an info panel with some text, with some more code below"))
+                           .paragraph("and an info panel with some text, with some more code below"))
                        .code(Language.javascript, "var i = 0;\nwhile(true) {\n  i++;\n}")
                        .paragraph("And a card")
                        .card("With a title", c -> c.attrs()
@@ -177,17 +183,17 @@ public final class MessageUtil {
                                                    .description("With some description, and a couple of attributes")
                                                    .background("https://www.atlassian.com")
                                                    .detail(d -> d
-                                                           .title("Type")
-                                                           .text("Task")
-                                                           .icon("https://ecosystem.atlassian.net/secure/"
-                                                                   + "viewavatar?size=xsmall&avatarId"
-                                                                   + "=15318&avatarType=issuetype", "Task"))
+                                                       .title("Type")
+                                                       .text("Task")
+                                                       .icon("https://ecosystem.atlassian.net/secure/"
+                                                           + "viewavatar?size=xsmall&avatarId"
+                                                           + "=15318&avatarType=issuetype", "Task"))
                                                    .detail(d -> d
-                                                           .title("User")
-                                                           .text("Joe Blog")
-                                                           .icon("https://ecosystem.atlassian.net/secure/"
-                                                                   + "viewavatar?size=xsmall&avatarId="
-                                                                   + "15318&avatarType=issuetype", "Task")));
+                                                       .title("User")
+                                                       .text("Joe Blog")
+                                                       .icon("https://ecosystem.atlassian.net/secure/"
+                                                           + "viewavatar?size=xsmall&avatarId="
+                                                           + "15318&avatarType=issuetype", "Task")));
     }
 
 
