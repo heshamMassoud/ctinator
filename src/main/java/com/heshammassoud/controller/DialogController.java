79e87c48e82e29a18eb0d5b305a93cce516f0be6
Hesham Massoud <hesham.massoud@commercetools.de>
2018-06-21 17:12:45 +0200

    @AuthorizeJwtParameter.

--- src/main/java/com/heshammassoud/controller/DialogController.java
+++ src/main/java/com/heshammassoud/controller/DialogController.java
@@ -2,6 +2,7 @@ package com.heshammassoud.controller;
 
 
 import com.atlassian.stride.spring.auth.AuthorizeJwtHeader;
+import com.atlassian.stride.spring.auth.AuthorizeJwtParameter;
 import com.heshammassoud.service.commercetools.ProductService;
 import io.sphere.sdk.products.Product;
 import org.springframework.stereotype.Controller;
@@ -29,6 +30,7 @@ public class DialogController {
      * @return a {@link ModelAndView} containing the appName and the serviceConfig.
      */
     @AuthorizeJwtHeader
+    @AuthorizeJwtParameter
     @GetMapping( {"/productdialog/", "/productdialog"})
     public String renderDialogView(@RequestParam("id") final String id,
                                    @Nonnull final Model model) {
